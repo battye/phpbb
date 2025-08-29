a57a92c4dee08831c270855ef0fb2b75e3cf4b3b
battye <administration@online-scrabble.com>
2025-08-29 14:09:50 +0000

    Change order

--- .devcontainer/customisations-team/setup.sh
+++ .devcontainer/customisations-team/setup.sh
@@ -40,18 +40,6 @@ URL_EPV="https://github.com/phpbb/epv"
 URL_TV="https://github.com/battye/phpbb-translation-validator"
 URL_TITANIA="https://github.com/battye/customisation-db.git"
 
-# Install phpBB
-echo "[Codespaces] Run phpBB CLI installation"
-cd /workspaces/phpbb/phpBB && composer install --no-interaction
-sudo php /workspaces/phpbb/phpBB/install/phpbbcli.php install /tmp/phpbb-config.yml
-rm -rf /workspaces/phpbb/phpBB/install
-
-# Set to debug mode
-sed -i \
-  -e "s|@define('PHPBB_ENVIRONMENT', 'production');|@define('PHPBB_ENVIRONMENT', 'development');|" \
-  -e "s|// *@define('DEBUG_CONTAINER', true);|@define('DEBUG_CONTAINER', true);|" \
-  /workspaces/phpbb/phpBB/config.php
-
 # Install Titania
 echo "[Codespaces] Install Titania"
 cd /workspaces/
@@ -80,6 +68,18 @@ cd /workspaces/translation-validator
 composer install --no-interaction
 rm -rf /workspaces/phpbb/phpBB/ext/phpbb/titania/vendor/phpbb/translation-validator
 sudo ln -s /workspaces/translation-validator /workspaces/phpbb/phpBB/ext/phpbb/titania/vendor/phpbb/translation-validator
+
+# Install phpBB (last so that the extension is auto-installed)
+echo "[Codespaces] Run phpBB CLI installation"
+cd /workspaces/phpbb/phpBB && composer install --no-interaction
+sudo php /workspaces/phpbb/phpBB/install/phpbbcli.php install /tmp/phpbb-config.yml
+rm -rf /workspaces/phpbb/phpBB/install
+
+# Set to debug mode
+sed -i \
+  -e "s|@define('PHPBB_ENVIRONMENT', 'production');|@define('PHPBB_ENVIRONMENT', 'development');|" \
+  -e "s|// *@define('DEBUG_CONTAINER', true);|@define('DEBUG_CONTAINER', true);|" \
+  /workspaces/phpbb/phpBB/config.php
 
 # Finished
 echo "[Codespaces] phpBB (Customisation Team) installation completed"
